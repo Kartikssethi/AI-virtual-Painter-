dccc333ce45bb2cce216eb2f3ffb364518e5d12e
Arhaan Bhiwandkar <arhaanbhiwandkar@gmail.com>
2025-02-22 18:02:00 +0530

    added a canvas in which user will paint drawing will not be made outside the canvas

--- main.py
+++ main.py
@@ -11,20 +11,6 @@ folderPath = "Header"
 myList = os.listdir(folderPath)
 print(myList)
 
-overlayList = []
-for imPath in myList:
-    if imPath.lower().endswith((".png", ".jpg", ".jpeg")):  # Ensure its an image
-        image = cv2.imread(f'{folderPath}/{imPath}')
-        if image is not None:  # Check if image is loaded correctly
-            image = cv2.resize(image, (1280, 250))  # Resize images to match header size
-            overlayList.append(image)
-
-print(len(overlayList))
-if overlayList:  # Ensure at least one image is loaded
-    header = overlayList[0]  # Giving an initial value
-else:
-    raise Exception("No valid images found in the 'Header' folder!")
-
 drawColor = (0, 255, 0)
 
 cap = cv2.VideoCapture(0)
@@ -52,27 +38,10 @@ while True:
         # 3. Check which fingers are up
         fingers = detector.fingersUp()  # Correct function call
 
-        # 4. If selection mode - two fingers are up
-        if fingers[1] and fingers[2]:
-            print("Selection mode")
-            # Checking for the click
-            if y1 < 250:  # Ensure within header range
-                if 250 < x1 < 450:
-                    header = overlayList[0]
-                    drawColor = (0, 255, 0)
-                elif 550 < x1 < 750:
-                    header = overlayList[1]
-                    drawColor = (255, 0, 0)
-                elif 800 < x1 < 950:
-                    header = overlayList[2]
-                    drawColor = (255, 0, 255)
-                elif 1050 < x1 < 1250:
-                    header = overlayList[3]
-                    drawColor = (0, 0, 0)
-            cv2.rectangle(img, (x1, y1 - 15), (x2, y2 + 15), drawColor, cv2.FILLED)
+        
 
         # 5. If drawing mode - Index finger is up
-        if fingers[1] and fingers[2] == False:
+        if fingers[1] and fingers[2] == False and x1>700 and x1<1200 and y1>50 and y1<600:
             cv2.circle(img, (x1, y1), 15, (0, 255, 0), cv2.FILLED)
             print("Drawing mode")
             if xp == 0 and yp == 0:
@@ -87,7 +56,7 @@ while True:
             xp, yp = x1, y1
 
     # Setting the header image
-    img[0:250, 0:1280] = header
+    img[50:600, 700:1200] = (255,255,255)
     img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0) #merge 
     cv2.imshow("Image", img)
     cv2.imshow("Canvas", imgCanvas)
