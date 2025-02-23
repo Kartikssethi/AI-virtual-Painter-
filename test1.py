ea2fcb561c82afd10e4dc7e3f39d0de82b2797d2
Kartikssethi <kartikssethi@gmail.com>
2025-02-23 13:02:55 +0530

    new test file and new test_header folder added

--- test1.py
+++ test1.py
@@ -7,16 +7,16 @@ import HandTracking as htm
 brush = 15
 eraser = 50
 
-folderPath = "Header"
+folderPath = "Test_Header"
 myList = os.listdir(folderPath)
 print(myList)
 
 overlayList = []
 for imPath in myList:
-    if imPath.lower().endswith((".png", ".jpg", ".jpeg")):  # Ensure its an image
+    if imPath.lower().endswith((".png", ".jpg", ".jpeg")):  # Ensure it's an image
         image = cv2.imread(f'{folderPath}/{imPath}')
         if image is not None:  # Check if image is loaded correctly
-            image = cv2.resize(image, (1280, 250))  # Resize images to match header size
+            image = cv2.resize(image, (1280, 100))  # Resize to fit the bottom width
             overlayList.append(image)
 
 print(len(overlayList))
@@ -32,11 +32,11 @@ cap.set(3, 1280)
 cap.set(4, 720)
 
 detector = htm.HandTrackingModule(detectionCon=0.85)
-xp, yp = 800, 50
+xp, yp = 0, 0
 imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
 while True:
-    # 1. importing image
+    # 1. Import image
     success, img = cap.read()
     img = cv2.flip(img, 1)
 
@@ -52,49 +52,43 @@ while True:
         # 3. Check which fingers are up
         fingers = detector.fingersUp()  # Correct function call
 
-        # 4. If selection mode - two fingers are up
+        # 4. Selection mode
         if fingers[1] and fingers[2]:
             print("Selection mode")
-            # Checking for the click
-            if y1 < 250:  # Ensure within header range
-                if 250 < x1 < 450:
+            if x1 < 100:  # Detect clicks in the left-side header
+                if 400 < y1 < 450:
                     header = overlayList[0]
                     drawColor = (0, 255, 0)
-                elif 550 < x1 < 750:
+                elif 500 < y1 < 600:
                     header = overlayList[1]
-                    drawColor = (255, 0, 0)
-                elif 800 < x1 < 950:
-                    header = overlayList[2]
-                    drawColor = (255, 0, 255)
-                elif 1050 < x1 < 1250:
-                    header = overlayList[3]
                     drawColor = (0, 0, 0)
-            cv2.rectangle(img, (x1, y1 - 15), (x2, y2 + 15), drawColor, cv2.FILLED)
+
+            cv2.rectangle(img, (x1 - 15, y1), (x2 + 15, y2), drawColor, cv2.FILLED)
 
         # 5. If drawing mode - Index finger is up
-        if fingers[1] and fingers[2] == False:
+        if fingers[1] and not fingers[2] and 700 < x1 < 1200 and 50 < y1 < 600:
             cv2.circle(img, (x1, y1), 15, (0, 255, 0), cv2.FILLED)
             print("Drawing mode")
-            if (800<x1<1200):
-                
-                # if xp == 800 and yp ==50:
-                #     print("here")    
-
-                    xp, yp = x1, y1
-                    if drawColor == (0, 0, 0):
-                        cv2.line(img, (xp, yp), (x1, y1), drawColor, eraser)
-                        cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraser)
-                    else:
-                        cv2.line(img, (xp, yp), (x1, y1), drawColor, brush)
-                        cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brush)
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
+            if drawColor == (0, 0, 0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraser)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraser)
+            else:
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brush)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brush)
 
             xp, yp = x1, y1
 
     # Setting the header image
-    # img[0:250, 0:1280] = header
-    # img[50:600,800:1200] = (255,0,0)
-    img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0) #merge 
+    img[50:600, 700:1200] = (255, 255, 255)
+    img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)  # Merge
+    
+    # Resize header to fit the left-side placement
+    header_resized = cv2.resize(header, (100, 720))  # Resize to (width=100, height=720)
+    img[0:720, 0:100] = header_resized  # Place header on the left side
+
+    img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
+
     cv2.imshow("Image", img)
-
-
     cv2.waitKey(1)
